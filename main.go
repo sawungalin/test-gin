3f1bfb4996a802011c0dfc872ac1eb7b5205b90e
sawun galin <sgsawungalin@gmail.com>
2022-01-08 00:03:21 -0800

    localhost

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	port := os.Getenv("PORT")
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
